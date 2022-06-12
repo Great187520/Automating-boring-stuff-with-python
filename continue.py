f2db8374876b516e4b855d144db924e11606e051
Eleke Great Chima <59459106+Great187520@users.noreply.github.com>
2022-06-12 23:54:06 +0100

    Add files via upload

---
+++ continue.py
@@ -0,0 +1,11 @@
+while True:
+    print("hello what's your name")
+    name = input()
+    if name != 'Jeo':
+        continue
+    print("Hello JEO, nice to meet you, whats the password? it's a fish")
+    password = input()
+    if password == 'swordFish':
+        break
+print('Access Granted')
+    
---
+++ if_else.py
@@ -0,0 +1,11 @@
+name = input('Enter your name :')
+age = int(input('Enter your age :'))
+name.lower()
+if name == 'Alice'.lower():
+          print('welcome Alice')
+elif age < 12:
+    print('your are not alice kiddo')
+elif age> 2000:
+    print('Alice is not an undead or a vampire')
+elif age >100:
+    print ('your are not Alice grannie')
